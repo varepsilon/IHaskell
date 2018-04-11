f58cfffa1b82feccfa6a17fae01fbac5aa072038
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-11 14:53:44 +0800

    release.nix: provide jupyterlab, doesn't work though

--- release.nix
+++ release.nix
@@ -55,8 +55,41 @@ let
 
     } // displays self;
   };
+  pythonOverrides = self: super: {
+    jupyterlab = super.jupyterlab.overrideAttrs (oldAttrs: rec {
+      name = "jupyterlab-${version}";
+      version = "0.31.12";
+
+      src = nixpkgs.fetchurl {
+        url = "mirror://pypi/j/jupyterlab/${name}.tar.gz";
+        sha256 = "1hp6p9bsr863glildgs2iy1a4l99m7rxj2sy9fmkxp5zhyhqvsrz";
+      };
+
+      propagatedBuildInputs = with self; [ notebook jupyterlab_launcher ];
+    });
+
+    jupyterlab_launcher = self.buildPythonPackage rec {
+      name = "jupyterlab_launcher-${version}";
+      version = "0.10.5";
+
+      src = nixpkgs.fetchurl {
+        url = "mirror://pypi/j/jupyterlab_launcher/${name}.tar.gz";
+        sha256 = "1v1ir182zm2dl14lqvqjhx2x40wnp0i32n6rldxnm1allfpld1n7";
+      };
+
+      propagatedBuildInputs = with self; [ notebook jsonschema ];
+
+      doCheck = false;
+
+      meta = {
+        description = "This package is used to launch an application built using JupyterLab.";
+        license = with nixpkgs.lib.licenses; [ bsd3 ];
+        homepage = "http://jupyter.org/";
+      };
+    };
+  };
   ihaskellEnv = haskellPackages.ghcWithPackages (self: [ self.ihaskell ] ++ packages self);
-  jupyter = nixpkgs.python3.withPackages (ps: [ ps.jupyter ps.notebook ]);
+  jupyter = (nixpkgs.python3.override { packageOverrides = pythonOverrides; }).withPackages (ps: [ ps.jupyter ps.notebook ps.jupyterlab ]);
   ihaskellSh = cmd: extraArgs: nixpkgs.writeScriptBin "ihaskell-${cmd}" ''
     #! ${nixpkgs.stdenv.shell}
     export GHC_PACKAGE_PATH="$(echo ${ihaskellEnv}/lib/*/package.conf.d| tr ' ' ':'):$GHC_PACKAGE_PATH"
@@ -69,6 +102,7 @@ nixpkgs.buildEnv {
   buildInputs = [ nixpkgs.makeWrapper ];
   paths = [ ihaskellEnv jupyter ];
   postBuild = ''
+    ln -s ${ihaskellSh "lab" ""}/bin/ihaskell-lab $out/bin/
     ln -s ${ihaskellSh "notebook" ""}/bin/ihaskell-notebook $out/bin/
     ln -s ${ihaskellSh "nbconvert" ""}/bin/ihaskell-nbconvert $out/bin/
     ln -s ${ihaskellSh "console" "--kernel=haskell"}/bin/ihaskell-console $out/bin/
